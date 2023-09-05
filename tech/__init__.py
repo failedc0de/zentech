f8b2b94582ad05a04851ded7ce18a40515763554
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-05 22:22:28 +0700

    awalan

--- tech/__init__.py
+++ tech/__init__.py
@@ -1,1 +1,16 @@
+import asyncio
+from pyrogram import Client
+from pyromod import listen 
+from tech import config
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+MONGODB_CLI = MongoClient(" MongoDB ")
+db = MONGODB_CLI.userbot
+
+manage = Client(
+  name=":memory:",
+  api_id=config.API_ID,
+  api_hash=config.API_HASH,
+  bot_token=config.BOT_TOKEN,
+  plugins={"root": "tech.Manager"},
+)
