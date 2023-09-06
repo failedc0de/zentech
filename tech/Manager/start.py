d36ef81ffc3a72668fe6ff4e40dd31d4b1f912ff
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-06 14:14:05 +0700

    .

--- tech/Manager/start.py
+++ tech/Manager/start.py
@@ -3,6 +3,5 @@ from pyrogram.types import *
 from tech import manage
 
 @manage.on_message(filters.private & filters.command("start"))
-async def start_bot(zen: manage, msg: Message):
-    user_id = msg.chat.id
-    return await zen.send_message(user_id, f"👋🏻 Hallo {msg.from_user.mention}")
+async def start_bot(zen: manage, message: Message):
+    await zen.send_message(message.from_user.id, f"👋🏻 Hallo")
