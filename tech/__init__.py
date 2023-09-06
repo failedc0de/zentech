8f09ba1a9db0cf6965de627fb1a3bd910970a7d6
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-06 14:40:04 +0700

    Update __init__.py

--- tech/__init__.py
+++ tech/__init__.py
@@ -8,7 +8,7 @@ from tech import config
 # db = MONGODB_CLI.userbot
 
 manage = Client(
-  ":memory:",
+  name=":memory:",
   api_id=config.API_ID,
   api_hash=config.API_HASH,
   bot_token=config.BOT_TOKEN,
