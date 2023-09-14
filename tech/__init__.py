2654d7c1498c25e89a9529dfa48fc122e4948d64
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 19:14:47 +0700

    .

--- tech/__init__.py
+++ tech/__init__.py
@@ -2,10 +2,30 @@ import asyncio
 from pyrogram import Client
 from pyromod import listen 
 from tech import config
-# from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-# MONGODB_CLI = MongoClient(" MongoDB ")
-# db = MONGODB_CLI.userbot
+import pymysql
+ 
+ 
+# establish connection to MySQL
+connection = pymysql.connect(
+    # specify host name
+    host="localhost",
+    
+    # specify username
+    user="id21233481_khansa",
+    
+    # enter password for above user
+    password="Knsgnwn#1",
+     
+    # default port number for MySQL is 3306
+    port=3306,
+     
+    # specify database name
+    db="id21233481_zenblogger"
+)
+ 
+# make cursor for establish connection
+mycursor = connection.cursor()
 
 manage = Client(
   name=":memory:",
