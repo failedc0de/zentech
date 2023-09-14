8494a5475377cb94a3886bd3fd74c48ebb54c22d
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 21:25:24 +0700

    Update __main__.py

--- tech/__main__.py
+++ tech/__main__.py
@@ -2,7 +2,14 @@ import asyncio
 
 from pyrogram import Client, idle
 from pyromod import listen 
-from tech import manage
+
+manage = Client(
+  name=":memory:",
+  api_id=config.API_ID,
+  api_hash=config.API_HASH,
+  bot_token=config.BOT_TOKEN,
+  plugins={"root": "tech.Manager"},
+)
 
 async def start():
     await manage.start()
