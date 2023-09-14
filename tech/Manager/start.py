919e4154a98f8a06531b79af72a885319bcfc091
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 22:23:21 +0700

    Update start.py

--- tech/Manager/start.py
+++ tech/Manager/start.py
@@ -4,14 +4,14 @@ from tech import *
 
 
 @Client.on_message(filters.private & filters.command("start"))
-async def start_bot(_, msg: Message):
-    form = await _.send_message(msg.chat.id, f"hello")
+async def start_bot(_, ctx: Message):
+    form = await _.send_message(ctx.chat.id, f"hello")
     
 @Client.on_message(filters.private & filters.command("form"))
-async def form_bot(_, msg: Message):
+async def form_bot(_, ctx: Message):
     try:
-        cek = await _.ask(msg.chat.id, "kirim string")
-        await _.send_message(msg.chat.id, f"string kamu\n{cek}")
+        cek = await ctx.chat.ask("Type your string session")
+        await _.send_message(ctx.chat.id, f"string kamu\n{cek.teks}")
     except Exception as ex:
         print('Mistakes for connection')
         print(ex)
