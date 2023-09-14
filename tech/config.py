f81a689967965b70e9b65f62bff95212602a69e2
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 22:39:45 +0700

    Update config.py

--- tech/config.py
+++ tech/config.py
@@ -6,6 +6,6 @@ load_dotenv("config.env")
 
 API_ID = int(getenv("API_ID", "3563677"))
 API_HASH = getenv("API_HASH", "483c51115c56b7d44111bd0fe31cc9cf")
-BOT_TOKEN = getenv("BOT_TOKEN", "6166148172:AAF2yABKyEF3nOLroJIuzO7zSAKEvuaAJxg")
+BOT_TOKEN = getenv("BOT_TOKEN", "6488957061:AAEmI6bps5NV7rWGaw5FBBW2UiDdDlABS_o")
 #BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 #MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knsgnwn:knsgnwn@cluster0.jsdln69.mongodb.net/?retryWrites=true&w=majority")
