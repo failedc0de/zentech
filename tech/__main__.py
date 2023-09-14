82617a0a246617cbf61f9beaa967d28e44a15d25
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 19:04:32 +0700

    Update __main__.py

--- tech/__main__.py
+++ tech/__main__.py
@@ -2,20 +2,7 @@ import asyncio
 
 from pyrogram import Client, idle
 from pyromod import listen 
-import config
-# from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-
-# MONGODB_CLI = MongoClient(" MongoDB ")
-# db = MONGODB_CLI.userbot
-
-manage = Client(
-  name=":memory:",
-  api_id=config.API_ID,
-  api_hash=config.API_HASH,
-  bot_token=config.BOT_TOKEN,
-  plugins={"root": "tech.Manager"},
-)
-
+from tech import manage
 
 async def start():
     await manage.start()
