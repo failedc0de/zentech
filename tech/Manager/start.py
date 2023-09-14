5f22fdd71f3ca9805f7b795cb93497027296c41f
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 21:20:07 +0700

    Update start.py

--- tech/Manager/start.py
+++ tech/Manager/start.py
@@ -1,4 +1,3 @@
-import pymysql
 from pyrogram import Client, idle, filters
 from pyrogram.types import Message
 from tech import *
@@ -10,17 +9,9 @@ async def start_bot(_, msg: Message):
     
 @manage.on_message(filters.private & filters.command("form"))
 async def form_bot(_, msg: Message):
-    form = await _.send_message(msg.chat.id, f"tunggu sebentar")
-    a = msg.chat.id
     try:
-        connection = pymysql.connect(host='localhost', user='id21233481_khansa', database='id21233481_zenblogger', password='Knsgnwn#1')
-        print('success')
-        cursor = connection.cursor()
-        anj = f"INSERT INTO `dashboard_user` (chat_id) VALUES (a)"
-        cursor.execute(anj)
-        connection.commit()
-        connection.close() 
-        await form.edit("berhasil")
+        cek = await msg.ask("kirim string")
+        await _.send_message(msg.chat.id, f"string kamu\n{cek}")
     except Exception as ex:
         print('Mistakes for connection')
         print(ex)
