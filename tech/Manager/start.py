b5e2001592b78d3cdfac071b88f447ed1deea9df
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-09-14 21:28:35 +0700

    Update start.py

--- tech/Manager/start.py
+++ tech/Manager/start.py
@@ -3,11 +3,11 @@ from pyrogram.types import Message
 from tech import *
 
 
-@manage.on_message(filters.private & filters.command("start"))
+@Client.on_message(filters.private & filters.command("start"))
 async def start_bot(_, msg: Message):
     form = await _.send_message(msg.chat.id, f"hello")
     
-@manage.on_message(filters.private & filters.command("form"))
+@Client.on_message(filters.private & filters.command("form"))
 async def form_bot(_, msg: Message):
     try:
         cek = await msg.ask("kirim string")
